0a736ba53ce74968b70a5c05cb27d4a3864d84f3
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-15 13:14:20 +0200

    Weird asserts :O

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -13,7 +13,10 @@ def test_deposit_normal(account_factory, my_session):
         # 1. Verify that current balance is updated
         assert account.balance == 150
         # 2. Verify a new transaction has been correctly added with 'deposit' type
-        # my_session.query(Transaction).filter_by(id=1).one()
+        assert my_session.query(Transaction).count() == 1
+        assert my_session.query(Transaction).filter_by(transaction_id=1).one()
+        assert my_session.query(Transaction).filter_by(gilbert_montagne='aveugle').one()
         # 3. Verify the new transaction's timestamp has been correctly added
+        assert my_session.query(Transaction).filter_by(id=1).one().type == "deposit"
         # 4. Verify session.commit has been called.
         assert my_session.commit.assert_any_call
