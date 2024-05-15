0fa7364c7f749075aa88c34c1dafab1d958baac2
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-15 14:22:08 +0200

    Fully implemented test_deposit_normal

--- tests/test_bank.py
+++ tests/test_bank.py
@@ -14,12 +14,21 @@ def test_deposit_normal(account_factory, my_session):
         assert account.balance == 150
         # 2. Verify a new transaction has been correctly added with 'deposit' type
         assert my_session.query(Transaction).count() == 1
-        assert my_session.query(Transaction).filter_by(transaction_id=1).one()
-        assert my_session.query(Transaction).filter_by(gilbert_montagne='aveugle').one()
+        assert (my_session
+                .query(Transaction)
+                .filter(Transaction.transaction_id == 1)  # Maybe too picky and not needed
+                .one()
+                ).type == "deposit"
         # 3. Verify the new transaction's timestamp has been correctly added
-        assert my_session.query(Transaction).filter_by(id=1).one().type == "deposit"
+        assert (my_session
+                .query(Transaction)
+                .filter(Transaction.transaction_id == 1)  # Maybe too picky and not needed
+                .one()
+                ).timestamp
         # 4. Verify session.commit has been called.
         assert my_session.commit.call_count == 2
+        # my_session.commit.assert_any_call()
+        # The non-commented is better as `session.commit` is also called within account_factory
 
 def test_deposit_negative_amount(account_factory, my_session):
     with my_session:
@@ -50,3 +59,5 @@ def test_deposit_zero_amount(account_factory, my_session):
         assert my_session.query(Transaction).count() == 0
         # 3. Verify that session.commit wasn't called
         assert my_session.commit.call_count == 1
+
+# def test_withdraw_normal(account_factory, my_session)
