b4524c4f6e2d82b92d4226f0a52b135e97ef6074
GMartin-Data <gregory.martin.data@gmail.com>
2024-05-15 12:01:36 +0200

    Succesfully tested that commit was called on normal deposit

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,19 @@ from src.bank import Account
 
 @pytest.fixture
 def my_session():
-    return UnifiedAlchemyMagicMock()
+    session = UnifiedAlchemyMagicMock()
+    yield session
+    session.rollback()
 
 @pytest.fixture
-def account_factory():
-    def create_account(account_id, balance, session):
-        return Account(
-            session = session,
+def account_factory(my_session):
+    def create_account(account_id, balance):
+        new_account = Account(
+            session = my_session,
             account_id = account_id,
             balance = balance
         )
+        my_session.add(new_account)
+        my_session.commit()
+        return new_account
     return create_account
--- tests/test_bank.py
+++ tests/test_bank.py
@@ -1,8 +1,10 @@
+from src.bank import Transaction
+
+
 def test_deposit_normal(account_factory, my_session):
     """Simple test with positive amount"""
     with my_session:
         account = account_factory(
-            session = my_session,
             account_id = 1,
             balance = 100
         )
@@ -11,7 +13,7 @@ def test_deposit_normal(account_factory, my_session):
         # 1. Verify that current balance is updated
         assert account.balance == 150
         # 2. Verify a new transaction has been correctly added with 'deposit' type
+        # my_session.query(Transaction).filter_by(id=1).one()
         # 3. Verify the new transaction's timestamp has been correctly added
         # 4. Verify session.commit has been called.
-
-
+        assert my_session.commit.assert_any_call
